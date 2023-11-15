d920df4309e9afe8b8828d569fa1fd0ff890c0b1
Bibizot <rmkharkov@gmail.com>
2023-11-15 16:48:32 +0200

    - fixed auto camera filed of view

---
+++ Assets/Scripts/Misc/CameraAutoFieldOfView.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace Misc
+{
+    public class CameraAutoFieldOfView : MonoBehaviour
+    {
+        private Camera Cam => Camera.main;
+        private CameraConfig UsedCamConfig => CameraConfig.Instance;
+        
+        private void Start()
+        {
+            AutoSetCamera();
+        }
+
+        private void AutoSetCamera()
+        {
+            var curResolution = new Vector2(Screen.width, Screen.height);
+            
+            var screenFactor = curResolution.x / curResolution.y;
+
+            var fieldPlane = UsedCamConfig.MaximumFiledValue - UsedCamConfig.MinimumFieldValue;
+            var resPlane = UsedCamConfig.MinResolution - UsedCamConfig.MaxResolution;
+            var resPercent = 1 - (screenFactor - UsedCamConfig.MaxResolution) / resPlane;
+            var setField = fieldPlane * resPercent + UsedCamConfig.MinimumFieldValue;
+            
+            Cam.fieldOfView = setField;
+        }
+    }
+}
---
+++ Assets/Scripts/Misc/CameraConfig.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Misc
+{
+    [CreateAssetMenu(fileName = "CameraConfig", menuName = "Configs/CameraConfig", order = 0)]
+    public class CameraConfig : ScriptableObject
+    {
+        private static CameraConfig _instance;
+
+        public static CameraConfig Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = Resources.Load<CameraConfig>("CameraConfig");
+                }
+
+                return _instance;
+            }
+        }
+
+        [SerializeField] private Vector2 targetResolution;
+        [SerializeField] private Vector2 minResolution;
+        [SerializeField] private Vector2 maxResolution;
+        [SerializeField] private float minimumFiledValue;
+        [SerializeField] private float maximumFiledValue;
+
+        public float TargetResolution => targetResolution.x / targetResolution.y;
+        public float MinResolution => minResolution.x / minResolution.y;
+        public float MaxResolution => maxResolution.x / maxResolution.y;
+        public float MinimumFieldValue => minimumFiledValue;
+        public float MaximumFiledValue => maximumFiledValue;
+    }
+}
